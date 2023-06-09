83ec035e4e6addf4efdd9fc0ae4f432a9d0aff7e
Priyanshu Upadhyay <62770722+Priyanshu-U@users.noreply.github.com>
2023-06-09 13:51:46 +0530

    Solved 1146. Snapshot Array.cpp

--- 1146. Snapshot Array.cpp
+++ 1146. Snapshot Array.cpp
@@ -1,3 +1,4 @@
+# Method 1 - Using inbuilt upperbound function
 class SnapshotArray {
 public:
     int snap_id;
@@ -27,8 +28,67 @@ public:
             return 0;
         }
 
-        it--; // get the element just before the snap_id
+        it--;
 
         return it->second;
     }
 };
+
+# Method 2 - Creating own Binary Search 
+
+class SnapshotArray {
+public:
+    unordered_map<int,vector<pair<int,int>>> snapArray;
+    int snapId = 0;
+    SnapshotArray(int length) {
+    }
+    
+    void set(int index, int val) {
+        snapArray[index].push_back({snapId,val});
+    }
+    
+    int snap() {
+        snapId++;
+        return snapId-1;
+    }
+    
+    int get(int index, int snap_id) {
+
+        auto itr = snapArray.find(index);
+        if (itr==snapArray.end()){
+            return 0;
+        }
+        vector<pair<int,int>> &values = itr->second;
+
+        int size = values.size();
+
+        int low = 0;
+        int high = size-1;
+
+        int mid;
+
+        int ans = 0;
+
+        while(low<=high){
+            mid = low + (high - low)/2;
+
+            if(values[mid].first<=snap_id){
+                ans = values[mid].second;
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return ans;
+
+    }
+};
+
+/**
+ * Your SnapshotArray object will be instantiated and called as such:
+ * SnapshotArray* obj = new SnapshotArray(length);
+ * obj->set(index,val);
+ * int param_2 = obj->snap();
+ * int param_3 = obj->get(index,snap_id);
+ */
