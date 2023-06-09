9289fbdcfdb2ffed0be882874cd0495cb515b27b
Priyanshu Upadhyay <62770722+Priyanshu-U@users.noreply.github.com>
2023-06-09 13:12:01 +0530

    Solved 981. Time Based Key-Value Store.cpp

--- 981. Time Based Key-Value Store.cpp
+++ 981. Time Based Key-Value Store.cpp
@@ -1,32 +1,41 @@
+# Method 1 - Using a Map of string:vector<pair<int,string>> and using the monotonous nature of timestamps for binary search on that vector.
 class TimeMap {
 public:
-  TimeMap() {}
-
-  void set(string key, string value, int timestamp) {
-    time_map[key].push_back({timestamp, value});
-  }
-
-  string get(string key, int timestamp) {
-    auto itr = time_map.find(key);
-    if (itr == time_map.end())
-      return "";
-
-    std::vector<std::pair<int, string>> &values = itr->second;
-    int low = 0;
-    int high = values.size() - 1;
-    while (low <= high) {
-      int mid = (low + high) / 2;
-      if (values[mid].first < timestamp)
-        low = mid + 1;
-      else if (values[mid].first > timestamp)
-        high = mid - 1;
-      else
-        return values[mid].second;
+    unordered_map<string,vector<pair<int,string>>> tm;
+    TimeMap() {
+        
     }
+    
+    void set(string key, string value, int timestamp) {
+        tm[key].push_back({timestamp,value});
+    }
+    
+    string get(string key, int timestamp) {
+ 
+        auto itr = tm.find(key);
+        if (itr == tm.end())
+        return "";
+        std::vector<std::pair<int, string>> &values = itr->second;
+        // Make sure to use a referrence instead of copying or calling the values from the map directly everytime, it increases the complexity, space and time, significantly.
+        
 
-    return high >= 0 ? values[high].second : "";
-  }
+        int size = values.size();
+        int low = 0;
+        int high = size - 1;
+        int mid;
+        string ans="";
 
-private:
-  std::unordered_map<string, std::vector<std::pair<int, string>>> time_map;
-}; 
+        while(low<=high){
+            mid = low + (high - low)/2;
+            if(values[mid].first<=timestamp){
+                ans = values[mid].second;
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return ans;
+        
+    }
+};
